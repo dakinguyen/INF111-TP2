9507dedbec6d84beac43a94e8faa1a47e131160d
dakinguyen <44127008+dakinguyen@users.noreply.github.com>
2019-03-10 23:38:42 -0400

    Update ProgrammePrincipal.java

--- src_gab/programme/ProgrammePrincipal.java
+++ src_gab/programme/ProgrammePrincipal.java
@@ -1,3 +1,11 @@
+/**
+ * Le programme principal du projet.
+ * 
+ * @author Dat Quang Nguyen, Sara Nguyen, Emerick Paul, Gabriel Somma
+ * @version Hiver 2019
+ *
+ */
+
 package programme;
 
 import java.io.IOException;
@@ -11,9 +19,9 @@ import utilitaires.*;
 public class ProgrammePrincipal {
 
 	/**
-	 * Programme principale, instancie les éléments de la simulation,
-	 * les lie entre eux, puis lance la séquence de test.
-	 * @param args, pas utilisé
+	 * Programme principale, instancie les Ã©lÃ©ments de la simulation,
+	 * les lie entre eux, puis lance la sÃ©quence de test.
+	 * @param args, pas utilisÃ©
 	 */
 	public static void main(String[] args){
 
@@ -41,55 +49,6 @@ public class ProgrammePrincipal {
 		rover.start();
 
 		satellite.start();
-
-
-
-
-
-
-
-		//testVect2D();
-		//testFileSimple();
 		
 	}
-
-	public static void testVect2D() {
-		Vect2D vecteur1 = new Vect2D(3,4);
-		Vect2D vecteur2 = new Vect2D(vecteur1);
-
-		System.out.println(vecteur1.getLongueur());
-		System.out.println(vecteur1.getAngle());
-
-		vecteur2.ajouter(3,4);
-		System.out.println(vecteur2.toString());
-
-		Vect2D resultant = vecteur1.calculerDiff(vecteur2);
-
-		System.out.println(vecteur1.equals(resultant));
-
-		vecteur2.diviser(2);
-		System.out.println(vecteur1.equals(vecteur2));
-	}
-
-	public static void testFileSimple() {
-		FileSimple file = new FileSimple();
-
-		int index = 0;
-
-		while (index < 5) {
-			file.ajouterElement(new Message(index));
-			index++;
-			System.out.println(index);
-		}
-
-		while (index > -1) {
-			try{
-				file.enleverElement();
-			}catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}
-
-	}
-
 }
