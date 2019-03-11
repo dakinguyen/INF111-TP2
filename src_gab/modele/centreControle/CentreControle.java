cd587695d6c306c0cf2bf3e9659171e6c166b444
dakinguyen <44127008+dakinguyen@users.noreply.github.com>
2019-03-10 23:39:23 -0400

    Update CentreControle.java

--- src_gab/modele/centreControle/CentreControle.java
+++ src_gab/modele/centreControle/CentreControle.java
@@ -4,6 +4,21 @@ import modele.communication.Message;
 import modele.communication.TransporteurMessage;
 import modele.satelliteRelai.SatelliteRelai;
 
+/**
+ * Cette classe definit le Centre de Controle
+ * 
+ * Elle herite de la classe message et contient les informations necessaires a l'envoie
+ * d'un message et a la gestion des messages
+ * 
+ * Service offert:
+ * - envoyerMessage
+ * - gestionnaireMessage
+ * 
+ * @author Dat Quang Nguyen, Sara Nguyen, Emerick Paul, Gabriel Somma
+ * @version Hiver 2019
+ *
+ */
+
 public class CentreControle extends TransporteurMessage {
 
     private SatelliteRelai satellite;
